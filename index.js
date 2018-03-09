26752ac17c51cf04ad60d0abb4645a83ed2e9b2c
David Park <dysp92@gmail.com>
2018-03-09 09:20:35 -0800

    created mockdata templates and added delete endpoints

--- index.js
+++ index.js
@@ -15,8 +15,40 @@ const cats = [{
   age: 2,
   breed: 'Bengal',
   story: 'Thrown on the street'
+}, {
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+}, {
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
 }];
 const dogs = [{
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
+}, {
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
+}, {
   imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
   imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
   name: 'Zeus',
@@ -46,6 +78,13 @@ app.get('/api/dog', (req, res) => {
   res.json(dogs[0]);
 });
 
+app.delete('/api/cat', (req, res) => {
+  res.json(cats[0]);
+});
+
+app.delete('/api/dog', (req, res) => {
+  res.json(dogs[0]);
+});
 
 function runServer(port = PORT) {
   const server = app
